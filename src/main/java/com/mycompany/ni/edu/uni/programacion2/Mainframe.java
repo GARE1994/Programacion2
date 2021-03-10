48a5a95e479b0caa78c7f33305afc480787b2abe
GARE1994 <romerobulls23@gmail.com>
2021-03-10 08:02:39 -0600

    Agrege una "clase de 10/03/2021"

--- src/main/java/com/mycompany/ni/edu/uni/programacion2/Mainframe.java
+++ src/main/java/com/mycompany/ni/edu/uni/programacion2/Mainframe.java
@@ -7,6 +7,8 @@ package com.mycompany.ni.edu.uni.programacion2;
 
 import java.awt.HeadlessException;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import javax.swing.SwingUtilities;
 
 /**
@@ -14,12 +16,19 @@ import javax.swing.SwingUtilities;
  * @author Sistemas-12
  */
 public class Mainframe extends JFrame {
+    
+    private JLabel titlelabel;
 
     public Mainframe() {
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300,400);
         setTitle("My first frame");
+        setVisible(true);
+        
+        titlelabel = new JLabel("HOLA  MUNDO");
+        
+        getContentPane().add(titlelabel);
         setVisible(true);
 
     }
