04243178c28b70a3aa0a9337a7f6d33ebfe0c81b
Sistemas-01 <Sistemas-01@10.9.13.190>
2021-03-11 08:32:36 -0600

    se agrego dos paquetes mas

--- src/main/java/com/mycompany/ni/edu/uni/programacion2/starframe.java
+++ src/main/java/com/mycompany/ni/edu/uni/programacion2/starframe.java
@@ -5,17 +5,24 @@
  */
 package com.mycompany.ni.edu.uni.programacion2;
 
+import com.mycompany.ni.edu.uni.programacion2.views.panels.PnlCalculadora;
+import java.awt.BorderLayout;
+
 /**
  *
  * @author Sistemas-12
  */
 public class starframe extends javax.swing.JFrame {
+    
+    private PnlCalculadora pnlCalculadora;
 
     /**
      * Creates new form starframe
      */
     public starframe() {
+
         initComponents();
+        
     }
 
     /**
@@ -27,22 +34,58 @@ public class starframe extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        PnlButtons = new javax.swing.JPanel();
+        btncalculadora = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        PnlContent = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("MyApp");
+        getContentPane().setLayout(new java.awt.BorderLayout(3, 2));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        PnlButtons.setBackground(new java.awt.Color(255, 204, 204));
+        PnlButtons.setPreferredSize(new java.awt.Dimension(120, 100));
+        PnlButtons.setLayout(new java.awt.GridLayout(3, 0, 0, 8));
 
-        pack();
+        btncalculadora.setFont(new java.awt.Font("Yu Gothic Medium", 3, 14)); // NOI18N
+        btncalculadora.setText("Calculadora");
+        btncalculadora.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btncalculadoraActionPerformed(evt);
+            }
+        });
+        PnlButtons.add(btncalculadora);
+
+        jButton2.setText("jButton2");
+        PnlButtons.add(jButton2);
+
+        jButton3.setText("jButton3");
+        PnlButtons.add(jButton3);
+
+        getContentPane().add(PnlButtons, java.awt.BorderLayout.LINE_START);
+
+        PnlContent.setBackground(new java.awt.Color(255, 255, 255));
+        PnlContent.setLayout(new java.awt.BorderLayout(5, 5));
+        getContentPane().add(PnlContent, java.awt.BorderLayout.CENTER);
+
+        setSize(new java.awt.Dimension(416, 339));
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btncalculadoraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btncalculadoraActionPerformed
+        if (pnlCalculadora == null) {
+            pnlCalculadora = new PnlCalculadora();
+        }
+        if (PnlContent.getComponentCount() > 0 ) {
+            PnlContent.remove(0);
+        }
+        PnlContent.add(pnlCalculadora, BorderLayout.CENTER); 
+        
+        this.validate();
+        
+        //setVisible(true);
+    }//GEN-LAST:event_btncalculadoraActionPerformed
 
     /**
      * @param args the command line arguments
@@ -80,5 +123,10 @@ public class starframe extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel PnlButtons;
+    private javax.swing.JPanel PnlContent;
+    private javax.swing.JButton btncalculadora;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     // End of variables declaration//GEN-END:variables
 }
